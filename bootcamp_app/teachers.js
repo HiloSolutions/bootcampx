eb4ecb876be136f694e51bdfb174c5351e2041b4
Lauren Johnston <hilotraining@outlook.com>
2023-02-08 18:08:22 -0700

    done bootcamp app

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,6 +7,9 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
+const cohortName = process.argv[2] || 'JUL02';
+const values = [`%${cohortName}%`];
+
 
 pool.query(`
 SELECT DISTINCT teachers.name AS teacher, cohorts.name as cohort
@@ -14,9 +17,9 @@ FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+WHERE cohorts.name LIKE $1
 ORDER BY teacher;
-`)
+`, values)
   .then(res => {
     res.rows.forEach(teacher => {
       console.log(`${teacher.cohort}: ${teacher.teacher}`);
